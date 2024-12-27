05c2621a7756eb365eaaeb636de1b6bb9ced7914
Aloke Desai <aloke@warp.dev>
2024-12-26 20:58:24 -0500

    Add is_colored function

--- src/loaders/directwrite.rs
+++ src/loaders/directwrite.rs
@@ -30,6 +30,8 @@ use pathfinder_geometry::line_segment::LineSegment2F;
 use pathfinder_geometry::rect::{RectF, RectI};
 use pathfinder_geometry::transform2d::Transform2F;
 use pathfinder_geometry::vector::{Vector2F, Vector2I};
+use winapi::shared::minwindef::TRUE;
+use winapi::um::dwrite_2::IDWriteFontFace2;
 use std::borrow::Cow;
 use std::ffi::OsString;
 use std::fmt::{self, Debug, Formatter};
@@ -200,6 +202,13 @@ impl Font {
         NativeFont {
             dwrite_font: self.dwrite_font.clone(),
             dwrite_font_face: self.dwrite_font_face.clone(),
+        }
+    }
+
+    pub fn is_colored(&self) -> bool {
+        unsafe {
+            let font_face = self.dwrite_font_face.as_ptr().cast::<IDWriteFontFace2>();
+            (*font_face).IsColorFont() == TRUE
         }
     }
 
